9d2e901d7bb7c0fce3ac0818ac6d2e552f496adc
Wongedan <47300852+wongedan123@users.noreply.github.com>
2019-02-09 21:36:18 +0700

    Add files via upload

---
+++ pemula.sh
@@ -0,0 +1,105 @@
+pkg update && pkg upgrad
+clear
+echo "INSTALLATION PAKET AKAN MEMAKAN WAKTU BEBERAPA MENIT MOHON BERSABAR :)"
+sleep 3
+termux-setup-storage
+echo "INSTALLATION START...."
+echo ""
+sleep 2
+echo "install python.."
+sleep 2
+echo ""
+pkg install python
+sleep 2
+echo ""
+echo "install python2.."
+echo ""
+sleep 2
+pkg install python2
+echo ""
+sleep 2
+echo "install python3.."
+echo ""
+sleep 2
+pkg install python3
+echo ""
+sleep 2
+echo "install perl.."
+sleep 2
+echo ""
+pkg install perl
+echo ""
+sleep 2
+echo "install nano.."
+echo ""
+sleep 2
+pkg install nano
+echo ""
+sleep 2
+echo "install php.."
+echo ""
+sleep 2
+pkg install php
+echo ""
+sleep 2
+echo "install git.."
+echo ""
+sleep 2
+pkg install git
+echo ""
+sleep 2
+echo "install vim.."
+echo ""
+sleep 2
+pkg install vim
+echo ""
+sleep 2
+echo "install wget.."
+echo ""
+sleep 2
+pkg install wget
+echo ""
+sleep 2
+echo "install ruby.."
+echo ""
+sleep 2
+pkg install ruby
+echo ""
+sleep 2
+echo "install clang.."
+echo ""
+sleep 2
+pkg install clang
+echo ""
+sleep 2
+echo "install cowsay.."
+echo ""
+sleep 2
+pkg install cowsay
+echo ""
+sleep 2
+echo "install figlet.."
+echo ""
+sleep 2
+pkg install figlet
+echo ""
+sleep 2
+echo "install toilet.."
+echo ""
+sleep 2
+pkg install toilet
+echo ""
+sleep 2
+echo "install lolcat.."
+echo ""
+sleep 2
+gem install lolcat
+echo ""
+sleep 2
+clear
+echo "INSTALLATION SELESAI !"
+echo ""
+echo "silahkan ketik python2 php nik.php untuk memasuki program"
+echo ""
+sleep 2
+echo "INI HANYA UNTUK PERTAMA KALI INSTALL JIKA ANDA BARU MEMAKAINYA, JIKA INGIN MEMAKAI LAGI CUKUP KETIK php nik.php"
